9d8770767fe9010ea18ddce5eda50e23da4a4aff
smrutinjay <smrutinjaysahu@gmail.com>
2025-12-13 13:59:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(120), nullable=False)
+    password_hash = db.Column(db.String(500), nullable=False)
     role = db.Column(db.String(20), nullable=False, default='student') # 'admin' or 'student'
     full_name = db.Column(db.String(100))
     dob = db.Column(db.Date)
